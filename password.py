a1b88068126389010c0e1efe1ff18b56dc3c8f11
Kenneth-joseph <josephochieng812@gmail.com>
2019-12-14 15:05:46 +0300

    added the section for generating number of passwors

--- password.py
+++ password.py
@@ -15,7 +15,11 @@ class Password(object):
 new_password = Password("KENT", "09", "kent@gmail")
 print(new_password.email)
 
-password=input("into your password length required ")
-length=int(password)
-print(length*2)
-
+password = input("into your password length required ")
+length = int(password)
+char = 'abcdefghijklmnopqrstuvwxyz1234567890'
+for p in range(4):
+    passwordF=''
+    for c in range(length):
+     passwordF += random.choice(char)
+    print(passwordF)
