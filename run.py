b2680a1aef897c0f96425a51e2a844c960e1b343
Kenneth-joseph <josephochieng812@gmail.com>
2019-12-15 21:32:17 +0300

    added my saving credentials method

--- run.py
+++ run.py
@@ -6,3 +6,12 @@ def create_credentials(username, account, passwords):
     """
     this will help create new credentials
     """
+    new_credentials = User(username, account, passwords)
+    return new_credentials
+
+
+def save_credentials(credentials):
+    """
+    function to save the credentials created
+    """
+    credentials.save_credrntials()
