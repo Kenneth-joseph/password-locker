bd8c9ac811e2c0a3b302c54f3fb5c5495b626b35
Kenneth-joseph <josephochieng812@gmail.com>
2019-12-16 09:54:26 +0300

    added my shortcodes and inputs necessary

--- run.py
+++ run.py
@@ -36,10 +36,28 @@ def display_credential():
 def main():
     print("hello welcome to your password locker account, what is your name?")
     user_name = input()
+
     print(f"Hello {user_name}. what would you like to do?")
     print('\n')
 
     while True:
-        print("Use these short codes : cc - create a new credentials, dc - display credentials, fc -find a contact, "
-              "ex -exit the contact list ")
-
+        print("Use these short codes : cc - create a new credentials, dc - display credentials, fc -find a "
+              "credentials, "
+              "ex -exit the credentials list ")
+        short_code = input().lower()
+        if short_code == 'cc':
+            print("new credentials")
+            print("-" * 10)
+            print("username")
+            username = input()
+            print("enter the account")
+            account = input()
+            print("enter password")
+            password = input()
+            save_details(create_credentials(username, account, password))  # creating new credentials
+            print('\n')
+            print(f"new credentials for {account} is created")
+            print('\n')
+        elif short_code == 'dc':
+            if display_credential():
+                print("here is a list of all your credentials")
